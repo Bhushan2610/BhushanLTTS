026d067cbaca4c2ad7cfecf84df3c59df7f764e6
Bhushan2610 <86543711+Bhushan2610@users.noreply.github.com>
2021-07-15 00:31:38 +0530

    Create Project.h

---
+++ IMPLEMENTATION/inc/Project.h
@@ -0,0 +1,15 @@
+/**
+* @file project.h
+*
+*/
+
+ #ifndef __PROJECT_H
+ #define __PROJECT_H
+  
+ #include <stdio.h>
+  
+ int  temp=(1/er)+((1/(float)r2));
+ displayChoice();
+ void menu_display();
+  
+ #endif
